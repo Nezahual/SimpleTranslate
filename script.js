029fb7e53d0793b9788d824eef4080117b73c422
Nezahual <igmartinez87@gmail.com>
2018-01-03 20:35:22 +0100

    pruebas random

---
+++ script.js
@@ -0,0 +1,26 @@
+var cr= [];
+
+$(document).on({
+  'mouseup': function() {
+    cr= window.getSelection().getRangeAt(0).getClientRects();
+  },
+  'mousemove': function(ev) {
+    //hide the pop up
+    for(var i = 0 ; i < cr.length ; i++) {
+      if(ev.pageX >= cr[i].left && ev.pageX <= cr[i].right &&
+         ev.pageY >= cr[i].top  && ev.pageY <= cr[i].bottom
+        ) {
+            Materialize.toast(window.getSelection(), 4000);
+        break;
+      }
+      else{
+        var toastElement = $('.toast').first()[0];
+
+        if(toastElement != undefined){
+            var toastInstance = toastElement.M_Toast;
+            toastInstance.remove();
+        }
+      }
+    }
+  }
+});
